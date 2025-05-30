3288ff62df0eddd4807b3a6e5a942be2400b6390
lorenzo-benites <lorenzojosefb@gmail.com>
2025-05-30 01:21:43 -0300

    SellerService e aba de Sellers

---
+++ SalesAPI/Services/SellerService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesAPI.Data;
+using SalesAPI.Models;
+
+namespace SalesAPI.Services
+{
+    public class SellerService
+    {
+        private readonly SalesAPIContext _context;
+
+        public SellerService(SalesAPIContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
--- SalesAPI/Startup.cs
+++ SalesAPI/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesAPI.Data;
+using SalesAPI.Services;
 
 namespace SalesAPI
 {
@@ -40,6 +41,7 @@ namespace SalesAPI
                     options.UseMySql(Configuration.GetConnectionString("SalesAPIContext"), builder => builder.MigrationsAssembly("SalesAPI")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
