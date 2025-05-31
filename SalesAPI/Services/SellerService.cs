38fd0fc9d0a854f251982b3dc63f5c02437e2b92
lorenzo-benites <lorenzojosefb@gmail.com>
2025-05-30 22:48:24 -0300

    sellers Edit adicionado

--- SalesAPI/Services/SellerService.cs
+++ SalesAPI/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using SalesAPI.Data;
 using SalesAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using SalesAPI.Services.Exceptions;
 
 namespace SalesAPI.Services
 {
@@ -39,5 +40,23 @@ namespace SalesAPI.Services
             _context.Seller.Remove(obj);
             _context.SaveChanges();
         } 
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+        }
     }
 }
