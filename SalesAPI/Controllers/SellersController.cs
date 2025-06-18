eec4021562affc614b6ea4c5fa7bc4c9543dfd16
lorenzo-benites <lorenzojosefb@gmail.com>
2025-06-18 01:25:22 -0300

    mensagem de erro no delete seller

--- SalesAPI/Controllers/SellersController.cs
+++ SalesAPI/Controllers/SellersController.cs
@@ -70,8 +70,15 @@ namespace SalesAPI.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
-            await _sellerService.DeleteAsync(id);
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                await _sellerService.DeleteAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new { Message = e.Message });
+            }
         }
 
         public async Task<IActionResult> Details(int? id)
--- SalesAPI/Services/SellerService.cs
+++ SalesAPI/Services/SellerService.cs
@@ -36,10 +36,18 @@ namespace SalesAPI.Services
 
         public async Task DeleteAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-           await _context.SaveChangesAsync();
-        } 
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException(e.Message);
+            }
+        }
+    
 
         public async Task UpdateAsync(Seller obj)
         {
